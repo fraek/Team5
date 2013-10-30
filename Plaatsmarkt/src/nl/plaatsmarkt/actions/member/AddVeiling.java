866fc191cec6e5ae6f390e1c1593c260e9435b9b
thomasdik <ik@thomasdik.nl>
2013-10-30 13:02:44 +0100

    AddVeiling.java

--- Plaatsmarkt/src/nl/plaatsmarkt/actions/member/AddVeiling.java
+++ Plaatsmarkt/src/nl/plaatsmarkt/actions/member/AddVeiling.java
@@ -1,23 +1,50 @@
 package nl.plaatsmarkt.actions.member;
 
+import java.util.Map;
+
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.SessionAware;
+
+import nl.plaatsmarkt.domain.Gebruiker;
+import nl.plaatsmarkt.domain.SubCategorie;
+import nl.plaatsmarkt.domain.Veiling;
+import nl.plaatsmarkt.util.GebruikerAware;
+import nl.plaatsmarkt.util.IDAO;
+import nl.plaatsmarkt.util.ServiceProvider;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class AddVeiling extends ActionSupport {
+public class AddVeiling extends ActionSupport implements GebruikerAware, SessionAware{
 
 	private static final long serialVersionUID = 1L;
+	private IDAO<SubCategorie> subcatdao = ServiceProvider.getSubCategorieDAO();
+	private IDAO<Veiling> veilingdao = ServiceProvider.getVeilingDAO();
+	private IDAO<Gebruiker> gebruikerdao = ServiceProvider.getGebruikerDAO();
 	private String titel;
 	private String omschrijving;
 	private int subcats;
+	private SubCategorie subcategorie;
+	//session user
+	@SuppressWarnings("rawtypes")
+	private SessionMap session;
+	private Gebruiker SessionGebruiker, gebruiker;
 
 	@Override
 	public String execute() throws Exception {
-
+		SessionGebruiker = (Gebruiker)session.get("gebruiker");
+		setGebruiker((Gebruiker)gebruikerdao.getObject(SessionGebruiker.getID()));
+		//Veiling veiling = new Veiling();
+		subcategorie = (SubCategorie) subcatdao.getObject(subcats);
+		System.out.println(subcategorie);
+		System.out.println("subcategorie id: " + subcats + ", naam: ");
+		System.out.println("Gebruiker: " + gebruiker.getGebruikersnaam());
+		System.out.println("Veiling titel: " + titel + ", omschrijving: " + omschrijving);
 		return SUCCESS;
 	}
 
 	@Override
 	public void validate(){
-		System.out.println(subcats);
+		
 	}
 
 	public String getTitel() {
@@ -43,5 +70,34 @@ public class AddVeiling extends ActionSupport {
 	public void setSubcats(int subcats) {
 		this.subcats = subcats;
 	}
+	public Gebruiker getGebruiker() {
+		return gebruiker;
+	}
+
+	public void setGebruiker(Gebruiker gebruiker) {
+		this.gebruiker = gebruiker;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public void setSession(Map session) {
+		this.session = (SessionMap) session;
+	}
+
+	public Gebruiker getSessionGebruiker() {
+		return SessionGebruiker;
+	}
+
+	public void setSessionGebruiker(Gebruiker sessionGebruiker) {
+		SessionGebruiker = sessionGebruiker;
+	}
+
+	public SubCategorie getSubcategorie() {
+		return subcategorie;
+	}
+
+	public void setSubcategorie(SubCategorie subcategorie) {
+		this.subcategorie = subcategorie;
+	}
+	
 	
 }
