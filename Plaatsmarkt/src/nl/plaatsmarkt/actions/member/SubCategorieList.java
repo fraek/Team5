9405e82ab2d5f1d6f63b25fada9d23144e8945ce
Willem-Jan <rovers.wj@me.com>
2013-11-04 15:51:41 +0100

    ABC-Sort Subcategorie & Categorie

--- Plaatsmarkt/src/nl/plaatsmarkt/actions/member/SubCategorieList.java
+++ Plaatsmarkt/src/nl/plaatsmarkt/actions/member/SubCategorieList.java
@@ -1,8 +1,12 @@
 package nl.plaatsmarkt.actions.member;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
+import nl.plaatsmarkt.actions.guest.CategorieList.CustomComparator;
+import nl.plaatsmarkt.domain.Categorie;
 import nl.plaatsmarkt.domain.SubCategorie;
 import nl.plaatsmarkt.util.IDAO;
 import nl.plaatsmarkt.util.ServiceProvider;
@@ -18,11 +22,16 @@ public class SubCategorieList extends ActionSupport {
 	public String execute() throws Exception {
 		alleSubCategorieenByID = new ArrayList<SubCategorie>();
 		setAlleSubCategorieen(dao.read());
+		
+		
 		for(SubCategorie s:alleSubCategorieen){
 			if(s.getCategorie().getID() == id){
 				alleSubCategorieenByID.add(s);
 			}
 		}
+		
+		Collections.sort(alleSubCategorieen, new CustomComparator());
+		Collections.sort(alleSubCategorieenByID, new CustomComparator());
 		return SUCCESS;
 	}
 
@@ -48,5 +57,13 @@ public class SubCategorieList extends ActionSupport {
 
 	public void setId(int iD) {
 		id = iD;
+	}
+	
+	public class CustomComparator implements Comparator<Object> {
+		public int compare(Object o1, Object o2) {
+            SubCategorie sc1 = (SubCategorie) o1;
+            SubCategorie sc2 = (SubCategorie) o2;
+            return sc1.getNaam().compareToIgnoreCase(sc2.getNaam());
+        }
 	}
 }
